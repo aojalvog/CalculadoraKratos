18421c243e4cb6c0f66c7366fddab7ca1e06a8e5
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-02 17:17:19 +0200

    docs(CalculadoraKratos): :memo: Añadido el JavaDoc

--- PracticaCalculadora/src/util/Menu.java
+++ PracticaCalculadora/src/util/Menu.java
@@ -6,6 +6,12 @@ public class Menu {
 
 	}
 
+	/**
+	 * Menú que vamos a utilizar para ver qué operaciones vamos a utilizar.
+	 * 
+	 * @return menu
+	 */
+
 	public static String menu() {
 		return "Selecciona la operación que quieres realizar:\n " + "Pulse 1 para realizar una suma.\n "
 				+ "Pulse 2 para realizar una resta.\n " + "Pulse 3 para realizar una multiplicación.\n "
