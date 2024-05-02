50ba42b41d13bbae47a881772f1e7aabba146ee4
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-02 16:41:34 +0200

    fix(CalculadoraKratos): :bug: Sustitución de SYSOUT por Logger

--- PracticaCalculadora/src/main/Main.java
+++ PracticaCalculadora/src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import operaciones.OperacionesMatematicas;
 import util.Menu;
@@ -9,55 +11,85 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Logger logger = Logger.getLogger(Main.class.getName());
+
 		Scanner sc = new Scanner(System.in);
-		int x;
-		int y;
-		int result;
+		double x;
+		double y;
+		double result;
 		int option;
 		boolean seguir = true;
 
-		while (seguir) {
-			System.out.println(Menu.menu());
-			option = sc.nextInt();
-			switch (option) {
+		if (logger.isLoggable(Level.INFO)) {
+			while (seguir) {
+				logger.info(Menu.menu());
+				option = sc.nextInt();
 
-			case 1:
-				System.out.println("Introduce el valor del primer sumando");
-				x = sc.nextInt();
-				System.out.println("Introduce el valor del segundo sumando");
-				y = sc.nextInt();
-				result = OperacionesMatematicas.add(x, y);
-				System.out.println(x + " + " + y + " = " + result);
-				break;
+				switch (option) {
 
-			case 2:
-				System.out.println("Introduce el valor del minuendo");
-				x = sc.nextInt();
-				System.out.println("Introduce el valor del sustraendo");
-				y = sc.nextInt();
-				result = OperacionesMatematicas.subtract(x, y);
-				System.out.println(x + " - " + y + " = " + result);
-				break;
+				case 1:
+					logger.info("Introduce el valor del primer sumando");
+					x = sc.nextDouble();
+					logger.info("Introduce el valor del segundo sumando");
+					y = sc.nextDouble();
+					result = OperacionesMatematicas.add(x, y);
+					logger.info(x + " + " + y + " = " + result);
+					break;
 
-			case 3:
-				System.out.println("Introduce el valor del primer producto");
-				x = sc.nextInt();
-				System.out.println("Introduce el valor del segundo producto");
-				y = sc.nextInt();
-				result = OperacionesMatematicas.multiply(x, y);
-				System.out.println(x + " * " + y + " = " + result);
-				break;
-			case 4:
-				System.out.println("Introduce el valor del dividendo");
-				x = sc.nextInt();
-				System.out.println("Introduce el valor del divisor");
-				y = sc.nextInt();
-				result = OperacionesMatematicas.multiply(x, y);
-				System.out.println(x + " / " + y + " = " + result);
-				break;
+				case 2:
+					logger.info("Introduce el valor del minuendo");
+					x = sc.nextDouble();
+					logger.info("Introduce el valor del sustraendo");
+					y = sc.nextDouble();
+					result = OperacionesMatematicas.subtract(x, y);
+					logger.info(x + " - " + y + " = " + result);
+					break;
 
-			default:
-				seguir = false;
+				case 3:
+					logger.info("Introduce el valor del primer producto");
+					x = sc.nextDouble();
+					logger.info("Introduce el valor del segundo producto");
+					y = sc.nextDouble();
+					result = OperacionesMatematicas.multiply(x, y);
+					logger.info(x + " * " + y + " = " + result);
+					break;
+
+				case 4:
+					logger.info("Introduce el valor del dividendo");
+					x = sc.nextDouble();
+					logger.info("Introduce el valor del divisor");
+					y = sc.nextDouble();
+					result = OperacionesMatematicas.divide(x, y);
+					logger.info(x + " / " + y + " = " + result);
+					break;
+
+				case 5:
+					logger.info("Introduce el número");
+					x = sc.nextDouble();
+					logger.info("Introduce el valor del exponente");
+					y = sc.nextDouble();
+					result = OperacionesMatematicas.exponent(x, y);
+					logger.info(x + " eleveado a " + y + " = " + result);
+
+					break;
+
+				case 6:
+					logger.info("Introduce el número del que quieres obtener la raíz cuadrada");
+					x = sc.nextDouble();
+					result = OperacionesMatematicas.squareRoot(x);
+					logger.info("La raíz cuadrada de " + x + " es " + result);
+					break;
+
+				case 7:
+					logger.info("Introduce el número del que quieres obtener el factorial");
+					x = sc.nextDouble();
+					result = OperacionesMatematicas.factorial(x);
+					logger.info(x + "! = " + result);
+					break;
+
+				default:
+					seguir = false;
+				}
 			}
 
 		}
