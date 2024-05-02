720741031e796f632174c69100606e1dbd909add
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-02 17:16:05 +0200

    docs(CalculadoraKratos): :memo: Añadido JavaDoc de los métodos de las operaciones

--- PracticaCalculadora/src/operaciones/OperacionesMatematicas.java
+++ PracticaCalculadora/src/operaciones/OperacionesMatematicas.java
@@ -5,31 +5,63 @@ public class OperacionesMatematicas {
 	private OperacionesMatematicas() {
 
 	}
-	// método suma
+
+	/**
+	 * Método para hacer la suma de dos números introducidos por el usuario
+	 * 
+	 * @param x primer valor
+	 * @param y segundo valor
+	 * @return resultado de la suma
+	 */
 
 	public static double add(double x, double y) {
 		return x + y;
 	}
 
-	// método resta
+	/**
+	 * Método para hacer la resta de dos números introducidos por el usuario
+	 * 
+	 * @param x minuendo
+	 * @param y subtraendo
+	 * @return resultado de la resta
+	 */
 
 	public static double subtract(double x, double y) {
 		return x - y;
 	}
 
-	// método multiplicación
+	/**
+	 * Método para calcular la multiplicación de dos números introducidos por el
+	 * usuario
+	 * 
+	 * @param x producto
+	 * @param y producto
+	 * @return resltado de la multiplicación
+	 */
 
 	public static double multiply(double x, double y) {
 		return x * y;
 	}
 
-	// método división
+	/**
+	 * Métood para calcular la división de dos números introducidos por el usuario
+	 * 
+	 * @param x dividendo
+	 * @param y divisor
+	 * @return resultado de la división
+	 */
 
 	public static double divide(double x, double y) {
 		return x / y;
 	}
 
-	// método exponente
+	/**
+	 * Método para calcular el resultado de elevar un número a un exponente
+	 * 
+	 * @param x número a elevar
+	 * @param y exponente
+	 * @return devuelve x^y
+	 */
 
 	public static double exponent(double x, double y) {
 
@@ -37,13 +69,23 @@ public class OperacionesMatematicas {
 
 	}
 
-	// método raíz square root
+	/**
+	 * Método para calcular la raíz cuadrada de un número
+	 * 
+	 * @param x número
+	 * @return raíz cuadrada del número
+	 */
 
 	public static double squareRoot(double x) {
 		return Math.sqrt(x);
 	}
 
-	// método calcular factorial
+	/**
+	 * Método para calcular el factorial de un número
+	 * 
+	 * @param number número introducido
+	 * @return factorial del número
+	 */
 
 	public static long factorial(double number) {
 		long result = 1;
